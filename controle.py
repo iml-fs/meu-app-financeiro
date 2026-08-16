19553e06fa15d98d54b51a59ee6674371dc2c08e
Isabella Mazoni Leonel <isabellamleonel@gmail.com>
2026-08-15 23:11:18 -0300

    Update controle.py

--- controle.py
+++ controle.py
@@ -126,7 +126,7 @@ if not st.session_state["logado"]:
             else:
                 st.error("E-mail ou senha incorretos!")
 
-       with aba_cadastro:
+    with aba_cadastro:
         st.subheader("Criar uma conta")
 
         nome_cadastro = st.text_input(
@@ -153,7 +153,6 @@ if not st.session_state["logado"]:
 
         if st.button("Criar minha conta"):
             st.info("Botão funcionando!")
-
 # ==========================================
 # 2. O APLICATIVO (O Cofre)
 # ==========================================
