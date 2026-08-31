de9b3a295f816cd49523ab6a5db5ffdfba75093c
Isabella Mazoni Leonel <isabellamleonel@gmail.com>
2026-08-31 14:38:38 -0300

    Adapta coluna ID para UUID

--- controle.py
+++ controle.py
@@ -645,7 +645,7 @@ else:
                 use_container_width=True, 
                 num_rows="dynamic",
                 column_config={
-                    "ID": st.column_config.NumberColumn("ID", disabled=True),
+                    "ID": st.column_config.TextColumn("ID", disabled=True),
                     "Data": st.column_config.DateColumn("Data", format="DD/MM/YYYY")
                 }
             )
